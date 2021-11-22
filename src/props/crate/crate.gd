08d343ff52c0a6724b5354dbc668940f95f9d920
Konrad Nowakowski <konrad.x92@gmail.com>
2021-11-22 21:14:06 +0000

    Add collision snap

--- src/props/crate/crate.gd
+++ src/props/crate/crate.gd
@@ -2,4 +2,5 @@ extends KinematicBody2D
 
 func _physics_process(p_delta: float) -> void:
 	var motion = Vector2(0, 10)
-	move_and_slide(motion, Vector2.UP, true)
+	var snap = Vector2(0, 4)
+	move_and_slide_with_snap(motion, snap, Vector2.UP, true)
