bd8d573ff50a97bcb48da3ece55cc27f1d3b84d1
Konrad Nowakowski <konrad.x92@gmail.com>
2021-11-25 19:51:27 +0000

    Add gravity const to crate

--- src/props/crate/crate.gd
+++ src/props/crate/crate.gd
@@ -1,6 +1,14 @@
 extends KinematicBody2D
 
+const GRAVITY = Vector2.DOWN * 400
+
+var linear_velocity = Vector2()
+
 func _physics_process(p_delta: float) -> void:
-	var motion = Vector2(0, 10)
-	var snap = Vector2(0, 4)
-	move_and_slide_with_snap(motion, snap, Vector2.UP, true)
+	# Update body motion
+	var lv = linear_velocity
+	var snap = Vector2(0, 4) if lv.y > 0 and is_on_floor() else Vector2()  
+	linear_velocity = move_and_slide_with_snap(lv, snap, Vector2.UP, true)
+	
+	# Apply gravity to the body
+	linear_velocity += GRAVITY * p_delta
