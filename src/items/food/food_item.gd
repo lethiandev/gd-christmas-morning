813acdc480fbfe0896300d9f5b546accdfa242c0
Konrad Nowakowski <konrad.x92@gmail.com>
2021-12-02 18:37:33 +0000

    Remove tool script from food items

--- src/items/food/food_item.gd
+++ src/items/food/food_item.gd
@@ -1,25 +1,14 @@
 extends Area2D
-tool
 
 export var score = 10
 
-export var sprite_frame: int \
-	setget set_sprite_frame, get_sprite_frame
-
 func _ready() -> void:
-	if not Engine.is_editor_hint():
-		$Tween.start_animation($Sprite)
+	$Tween.start_animation($Sprite)
 
 func interact_eat(p_player: Node2D) -> void:
 	$CollisionShape2D.disabled = true
 	$FollowTarget.follow(p_player)
 	$Tween.stop_all()
-
-func set_sprite_frame(p_frame: int) -> void:
-	$Sprite.frame = p_frame
-
-func get_sprite_frame() -> int:
-	return $Sprite.frame
 
 func _on_follow_done(p_player: Node2D):
 	queue_free()
--- src/items/food/tween.gd
+++ src/items/food/tween.gd
@@ -1,12 +1,11 @@
 extends Tween
 
-func start_animation(p_target: Node) -> void:
-	var from = Vector2(0.0, -2.0)
-	var to = Vector2(0.0, 2.0)
-	
-	remove_all()
-	interpolate_property(p_target, "position", from, to, 0.5, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
-	interpolate_property(p_target, "position", to, from, 0.5, Tween.TRANS_SINE, Tween.EASE_IN_OUT, 0.5)
+func start_animation(p_target: Sprite) -> void:
+	var offset = Vector2(0.0, 2.0)
+	interpolate_property(p_target, "position",
+		offset, offset * -1, 0.5, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	interpolate_property(p_target, "position",
+		offset * -1, offset, 0.5, Tween.TRANS_SINE, Tween.EASE_IN_OUT, 0.5)
 	start()
 	
 	# Randomize animation tick
