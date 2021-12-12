8b7377be353cd4b39c9784a1acb7be2f646996a5
Konrad Nowakowski <konrad.x92@gmail.com>
2021-12-12 19:49:35 +0000

    Add dialogue into intro

--- src/stages/bedroom/intro.gd
+++ src/stages/bedroom/intro.gd
@@ -2,3 +2,9 @@ extends AnimationPlayer
 
 func fade_in() -> void:
 	Transition.fade_in()
+
+func dialog_start() -> void:
+	Dialogue.dialog_start([
+		{ "who": "player", "what": "Oh no! I'm late!" },
+		{ "who": "player", "what": "What I'm going to do!" },
+	])
