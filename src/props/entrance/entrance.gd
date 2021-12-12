6247c8715a4a882ab16aa261775751c77bbacda9
Konrad Nowakowski <konrad.x92@gmail.com>
2021-12-12 12:33:01 +0000

    Add entrance prop

---
+++ src/props/entrance/entrance.gd
@@ -0,0 +1,17 @@
+extends Area2D
+tool
+
+export(PackedScene) var next_level
+
+export var locked = false \
+	setget set_locked, is_locked
+
+func set_locked(p_locked: bool) -> void:
+	$Sprite.frame = 1 if p_locked else 0
+	locked = p_locked
+
+func is_locked() -> bool:
+	return locked
+
+func interact(p_other: Node) -> void:
+	pass
