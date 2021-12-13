5f6d0f3583b1ee254d5868a3d270fa1ab8d7c8b3
Konrad Nowakowski <konrad.x92@gmail.com>
2021-12-13 21:01:28 +0000

    Rename set lock method

--- src/props/entrance/entrance.gd
+++ src/props/entrance/entrance.gd
@@ -4,9 +4,9 @@ tool
 export(PackedScene) var next_level
 
 export var locked = false \
-	setget set_locked, is_locked
+	setget set_lock, is_locked
 
-func set_locked(p_locked: bool) -> void:
+func set_lock(p_locked: bool) -> void:
 	$Sprite.frame = 1 if p_locked else 0
 	locked = p_locked
 
