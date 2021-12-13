68500a02b04de92eec7367ea38c7409f0786c7cf
Konrad Nowakowski <konrad.x92@gmail.com>
2021-12-13 21:52:55 +0000

    Add controls in intro dialog

--- src/stages/bedroom/intro.gd
+++ src/stages/bedroom/intro.gd
@@ -7,4 +7,6 @@ func dialog_start() -> void:
 	Dialogue.dialog_start([
 		{ "who": "player", "what": "Oh no! I'm late!" },
 		{ "who": "player", "what": "What I'm going to do!" },
+		{ "who": "player", "what": "Press arrows keys to move around." },
+		{ "who": "player", "what": "Press Z key to jump and X key to interact with objects." },
 	])
